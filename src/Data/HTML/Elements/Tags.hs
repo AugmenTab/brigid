6abf8ed50b98d207db7449d029579975066d3ed5
AugmenTab <tbaum92@live.com>
2023-11-27 18:06:14 -0600

    Adds element data tags from Blaze
    This adds one data tag for every element supported by the Blaze
    HTML package upon which this library will be dependent. These will
    be required for the type-level element/attribute validation to
    come.
    
    Note that this DOES NOT filter out deprecated elements - that will
    come at a later time, as each element/attribute is researched and
    decisions are made about those associations.

---
+++ src/Data/HTML/Elements/Tags.hs
@@ -0,0 +1,219 @@
+module Data.HTML.Elements.Tags
+  ( Anchor
+  , Abbreviation
+  , Address
+  , Area
+  , Article
+  , Aside
+  , Audio
+  , Bold
+  , Base
+  , BiDirectionalOverride
+  , Blockquote
+  , Body
+  , Break
+  , Button
+  , Canvas
+  , Caption
+  , Cite
+  , Code
+  , Column
+  , ColumnGroup
+  , Command
+  , DataList
+  , Description
+  , Delete
+  , Details
+  , Definition
+  , Division
+  , DescriptionList
+  , DescriptionTerm
+  , Emphasis
+  , Embed
+  , Fieldset
+  , FigureCaption
+  , Figure
+  , Footer
+  , Form
+  , H1
+  , H2
+  , H3
+  , H4
+  , H5
+  , H6
+  , Head
+  , Header
+  , HeadingGroup
+  , HorizontalRule
+  , Html
+  , Italic
+  , IFrame
+  , Image
+  , Input
+  , Insert
+  , KeyboardInput
+  , Keygen
+  , Label
+  , Legend
+  , ListItem
+  , Link
+  , Main
+  , Map
+  , Mark
+  , Menu
+  , MenuItem
+  , Meta
+  , Meter
+  , Nav
+  , NoScript
+  , Object
+  , OrderedList
+  , OptionGroup
+  , Option
+  , Output
+  , Paragraph
+  , Param
+  , PreformattedText
+  , Progress
+  , Quotation
+  , RubyParenthesis
+  , RubyText
+  , Ruby
+  , Sample
+  , Script
+  , Section
+  , Select
+  , Small
+  , Source
+  , Span
+  , Strong
+  , Style
+  , Subscript
+  , Summary
+  , Superscript
+  , Table
+  , TableBody
+  , TableDataCell
+  , TextArea
+  , TableFooter
+  , TableHeaderCell
+  , TableHeader
+  , Time
+  , Title
+  , TableRow
+  , Track
+  , Underline
+  , UnorderedList
+  , Variable
+  , Video
+  , WordBreakOpportunity
+  ) where
+
+data Anchor
+data Abbreviation
+data Address
+data Area
+data Article
+data Aside
+data Audio
+data Bold
+data Base
+data BiDirectionalOverride
+data Blockquote
+data Body
+data Break
+data Button
+data Canvas
+data Caption
+data Cite
+data Code
+data Column
+data ColumnGroup
+data Command
+data DataList
+data Description
+data Delete
+data Details
+data Definition
+data Division
+data DescriptionList
+data DescriptionTerm
+data Emphasis
+data Embed
+data Fieldset
+data FigureCaption
+data Figure
+data Footer
+data Form
+data H1
+data H2
+data H3
+data H4
+data H5
+data H6
+data Head
+data Header
+data HeadingGroup
+data HorizontalRule
+data Html -- includes DOCTYPE declaration
+data Italic
+data IFrame
+data Image
+data Input
+data Insert
+data KeyboardInput
+data Keygen
+data Label
+data Legend
+data ListItem
+data Link
+data Main
+data Map
+data Mark
+data Menu
+data MenuItem
+data Meta
+data Meter
+data Nav
+data NoScript
+data Object
+data OrderedList
+data OptionGroup
+data Option
+data Output
+data Paragraph
+data Param
+data PreformattedText
+data Progress
+data Quotation
+data RubyParenthesis
+data RubyText
+data Ruby
+data Sample
+data Script
+data Section
+data Select
+data Small
+data Source
+data Span
+data Strong
+data Style
+data Subscript
+data Summary
+data Superscript
+data Table
+data TableBody
+data TableDataCell
+data TextArea
+data TableFooter
+data TableHeaderCell
+data TableHeader
+data Time
+data Title
+data TableRow
+data Track
+data Underline
+data UnorderedList
+data Variable
+data Video
+data WordBreakOpportunity
