9b736ef41723c84245bc3819775718b268870c57
AugmenTab <tbaum92@live.com>
2023-11-27 18:03:43 -0600

    Adds utility type class and family to aid in using type lists
    This adds a type class `Elem` that checks for membership of a
    given type within a type list, and a type family that can append
    type lists. This will be required for the type stew that will allow
    us to type-check an attribute's validity on an element at compile
    time.

---
+++ src/Data/HTML/Types.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeFamilies #-}
+
+-- This is to prevent warnings for the non-matching case in the third `Elem`
+-- instance. GHC claims that this is a redundant constraint, but attributes
+-- will fail to compile without it. I believe this is related to a known GHC
+-- bug.
+{-# OPTIONS_GHC -fno-warn-redundant-constraints #-}
+
+module Data.HTML.Types
+  (
+  ) where
+
+import           Data.Kind (Type)
+
+class Elem (a :: Type) (list :: [Type]) where
+
+instance Elem a '[] where
+instance {-# OVERLAPPING #-} Elem a (a ': rest) where
+instance {-# OVERLAPPABLE #-} Elem a (b ': rest) where
+
+type family (xs :: [Type]) ++ (ys :: [Type]) :: [Type] where
+  '[]       ++ ys = ys
+  (x ': xs) ++ ys = x ': (xs ++ ys)
