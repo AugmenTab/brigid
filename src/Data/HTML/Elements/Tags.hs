5d2f406a82d078887fbfee7745e56e54e70edad3
AugmenTab <tbaum92@live.com>
2023-11-28 18:37:00 -0600

    Removes deprecated tags, adds documentation to those remaining
    This removes the elements that are listed as deprecated by the HTML
    documentation, and provides documentation explaining what each data
    tag represents for those that remain.
    
    Some elements that are listed as part of the HTML documentation are
    not present in the `blaze-html` library - I may fork the repo and
    open a PR to add them.

--- src/Data/HTML/Elements/Tags.hs
+++ src/Data/HTML/Elements/Tags.hs
@@ -19,10 +19,9 @@ module Data.HTML.Elements.Tags
   , Code
   , Column
   , ColumnGroup
-  , Command
   , DataList
   , Description
-  , Delete
+  , Deletion
   , Details
   , Definition
   , Division
@@ -50,7 +49,7 @@ module Data.HTML.Elements.Tags
   , IFrame
   , Image
   , Input
-  , Insert
+  , Insertion
   , KeyboardInput
   , Keygen
   , Label
@@ -61,7 +60,6 @@ module Data.HTML.Elements.Tags
   , Map
   , Mark
   , Menu
-  , MenuItem
   , Meta
   , Meter
   , Nav
@@ -72,7 +70,6 @@ module Data.HTML.Elements.Tags
   , Option
   , Output
   , Paragraph
-  , Param
   , PreformattedText
   , Progress
   , Quotation
@@ -109,111 +106,332 @@ module Data.HTML.Elements.Tags
   , WordBreakOpportunity
   ) where
 
+-- Other elements that are in the HTML 5 spec, but not in Blaze:
+--   - bdi
+--   - data
+--   - dialog
+--   - picture
+--   - s
+--   - search
+--   - slot
+--   - svg (maybe in the Blaze SVG library?)
+--   - template
+--
+-- These could perhaps be added in a PR to the `blaze-html` repo.
+
+-- | This data tag represents the @\<a>@ element.
 data Anchor
+
+-- | This data tag represents the @\<abbr>@ element.
 data Abbreviation
+
+-- | This data tag represents the @\<address>@ element.
 data Address
+
+-- | This data tag represents the @\<area>@ element.
 data Area
+
+-- | This data tag represents the @\<article>@ element.
 data Article
+
+-- | This data tag represents the @\<aside>@ element.
 data Aside
+
+-- | This data tag represents the @\<audio>@ element.
 data Audio
+
+-- | This data tag represents the @\<b>@ element.
 data Bold
+
+-- | This data tag represents the @\<base>@ element.
 data Base
+
+-- | This data tag represents the @\<bdo>@ element.
 data BiDirectionalOverride
+
+-- | This data tag represents the @\<blockquote>@ element.
 data Blockquote
+
+-- | This data tag represents the @\<body>@ element.
 data Body
+
+-- | This data tag represents the @\<br>@ element.
 data Break
+
+-- | This data tag represents the @\<button>@ element.
 data Button
+
+-- | This data tag represents the @\<canvas>@ element.
 data Canvas
+
+-- | This data tag represents the @\<caption>@ element.
 data Caption
+
+-- | This data tag represents the @\<cite>@ element.
 data Cite
+
+-- | This data tag represents the @\<code>@ element.
 data Code
+
+-- | This data tag represents the @\<col>@ element.
 data Column
+
+-- | This data tag represents the @\<colgroup>@ element.
 data ColumnGroup
-data Command
+
+-- | This data tag represents the @\<datalist>@ element.
 data DataList
+
+-- | This data tag represents the @\<dd>@ element.
 data Description
-data Delete
+
+-- | This data tag represents the @\<del>@ element.
+data Deletion
+
+-- | This data tag represents the @\<details>@ element.
 data Details
+
+-- | This data tag represents the @\<dfn>@ element.
 data Definition
+
+-- | This data tag represents the @\<div>@ element.
 data Division
+
+-- | This data tag represents the @\<dl>@ element.
 data DescriptionList
+
+-- | This data tag represents the @\<dt>@ element.
 data DescriptionTerm
+
+-- | This data tag represents the @\<em>@ element.
 data Emphasis
+
+-- | This data tag represents the @\<embed>@ element.
 data Embed
+
+-- | This data tag represents the @\<fieldset>@ element.
 data Fieldset
+
+-- | This data tag represents the @\<figcaption>@ element.
 data FigureCaption
+
+-- | This data tag represents the @\<figure>@ element.
 data Figure
+
+-- | This data tag represents the @\<footer>@ element.
 data Footer
+
+-- | This data tag represents the @\<form>@ element.
 data Form
+
+-- | This data tag represents the @\<h1>@ element.
 data H1
+
+-- | This data tag represents the @\<h2>@ element.
 data H2
+
+-- | This data tag represents the @\<h3>@ element.
 data H3
+
+-- | This data tag represents the @\<h4>@ element.
 data H4
+
+-- | This data tag represents the @\<h5>@ element.
 data H5
+
+-- | This data tag represents the @\<h6>@ element.
 data H6
+
+-- | This data tag represents the @\<head>@ element.
 data Head
+
+-- | This data tag represents the @\<header>@ element.
 data Header
+
+-- | This data tag represents the @\<hgroup>@ element.
 data HeadingGroup
+
+-- | This data tag represents the @\<hr>@ element.
 data HorizontalRule
-data Html -- includes DOCTYPE declaration
+
+{-| This data tag represents the @\<html>@ element. It will also apply the
+   documentation type declaration (@DOCTYPE HTML@).
+-}
+data Html
+
+-- | This data tag represents the @\<i>@ element.
 data Italic
+
+-- | This data tag represents the @\<iframe>@ element.
 data IFrame
+
+-- | This data tag represents the @\<img>@ element.
 data Image
+
+-- | This data tag represents the @\<input>@ element.
 data Input
-data Insert
+
+-- | This data tag represents the @\<ins>@ element.
+data Insertion
+
+-- | This data tag represents the @\<kbd>@ element.
 data KeyboardInput
+
+-- | This data tag represents the @\<keygen>@ element.
 data Keygen
+
+-- | This data tag represents the @\<label>@ element.
 data Label
+
+-- | This data tag represents the @\<legend>@ element.
 data Legend
+
+-- | This data tag represents the @\<li>@ element.
 data ListItem
+
+-- | This data tag represents the @\<link>@ element.
 data Link
+
+-- | This data tag represents the @\<main>@ element.
 data Main
+
+-- | This data tag represents the @\<map>@ element.
 data Map
+
+-- | This data tag represents the @\<mark>@ element.
 data Mark
+
+-- | This data tag represents the @\<menu>@ element.
 data Menu
-data MenuItem
+
+-- | This data tag represents the @\<meta>@ element.
 data Meta
+
+-- | This data tag represents the @\<meter>@ element.
 data Meter
+
+-- | This data tag represents the @\<nav>@ element.
 data Nav
+
+-- | This data tag represents the @\<noscript>@ element.
 data NoScript
+
+-- | This data tag represents the @\<object>@ element.
 data Object
+
+-- | This data tag represents the @\<ol>@ element.
 data OrderedList
+
+-- | This data tag represents the @\<optgroup>@ element.
 data OptionGroup
+
+-- | This data tag represents the @\<option>@ element.
 data Option
+
+-- | This data tag represents the @\<output>@ element.
 data Output
+
+-- | This data tag represents the @\<p>@ element.
 data Paragraph
-data Param
+
+-- | This data tag represents the @\<pre>@ element.
 data PreformattedText
+
+-- | This data tag represents the @\<progress>@ element.
 data Progress
+
+-- | This data tag represents the @\<q>@ element.
 data Quotation
+
+-- | This data tag represents the @\<rp>@ element.
 data RubyParenthesis
+
+-- | This data tag represents the @\<rt>@ element.
 data RubyText
+
+-- | This data tag represents the @\<ruby>@ element.
 data Ruby
+
+-- | This data tag represents the @\<samp>@ element.
 data Sample
+
+-- | This data tag represents the @\<script>@ element.
 data Script
+
+-- | This data tag represents the @\<section>@ element.
 data Section
+
+-- | This data tag represents the @\<select>@ element.
 data Select
+
+-- | This data tag represents the @\<small>@ element.
 data Small
+
+-- | This data tag represents the @\<source>@ element.
 data Source
+
+-- | This data tag represents the @\<span>@ element.
 data Span
+
+-- | This data tag represents the @\<strong>@ element.
 data Strong
+
+-- | This data tag represents the @\<style>@ element.
 data Style
+
+-- | This data tag represents the @\<sub>@ element.
 data Subscript
+
+-- | This data tag represents the @\<summary>@ element.
 data Summary
+
+-- | This data tag represents the @\<sup>@ element.
 data Superscript
+
+-- | This data tag represents the @\<table>@ element.
 data Table
+
+-- | This data tag represents the @\<tbody>@ element.
 data TableBody
+
+-- | This data tag represents the @\<td>@ element.
 data TableDataCell
+
+-- | This data tag represents the @\<textarea>@ element.
 data TextArea
+
+-- | This data tag represents the @\<tfoot>@ element.
 data TableFooter
+
+-- | This data tag represents the @\<th>@ element.
 data TableHeaderCell
+
+-- | This data tag represents the @\<thead>@ element.
 data TableHeader
+
+-- | This data tag represents the @\<time>@ element.
 data Time
+
+-- | This data tag represents the @\<title>@ element.
 data Title
+
+-- | This data tag represents the @\<tr>@ element.
 data TableRow
+
+-- | This data tag represents the @\<track>@ element.
 data Track
+
+-- | This data tag represents the @\<u>@ element.
 data Underline
+
+-- | This data tag represents the @\<ul>@ element.
 data UnorderedList
+
+-- | This data tag represents the @\<var>@ element.
 data Variable
+
+-- | This data tag represents the @\<video>@ element.
 data Video
+
+-- | This data tag represents the @\<wbr>@ element.
 data WordBreakOpportunity
