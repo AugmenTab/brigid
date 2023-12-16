88e81b28c0a5ea465f3e998e49d5bff1c3a9349f
AugmenTab <tbaum92@live.com>
2023-12-16 16:55:42 -0600

    Adds all straightforward ValidChildrenFor instances
    This includes all of the ValidChildrenFor instances that are
    reasonably straightforward to implement - a short list of specific
    elements, an entire content category, or some minor departure from
    those. Left out are those with conditional permitted content lists
    and those that are an entire content category with some exceptions.
    These will be added in the future.

--- src/Data/HTML4/Elements/TagGroups.hs
+++ src/Data/HTML4/Elements/TagGroups.hs
@@ -5,10 +5,9 @@ module Data.HTML4.Elements.TagGroups
   ( VoidElement
   , NoContent
   , NonElement
+  , TextOnly
   , Headings
-  , ListElements
-  , TableElements
-  , TableRowElements
+  , ListContent
   , MetadataContent
   , FlowContent
   , SectioningContent
@@ -22,6 +21,12 @@ module Data.HTML4.Elements.TagGroups
   , SubmittableContent
   , ResettableContent
   , ScriptSupportingContent
+  , LegendContent
+  , RubyContent
+  , SummaryContent
+  , TableContent
+  , TableRowContent
+  , TableRowOnly
   ) where
 
 import Data.HTML4.Elements.TagType (TagType(..))
@@ -57,34 +62,20 @@ type NoContent = '[]
 --
 type NonElement = '[]
 
+type TextOnly =
+  '[ Text ]
+
 type Headings =
-  [ H1
-  , H2
-  , H3
-  , H4
-  , H5
-  , H6
+  [ 'H1
+  , 'H2
+  , 'H3
+  , 'H4
+  , 'H5
+  , 'H6
   ]
 
-type ListElements =
-  [ ListItem
-  , Script
-  , Template
-  ]
-
-type TableElements =
-  [ Caption
-  , ColumnGroup
-  , TableHeader
-  , TableBody
-  , TableFooter
-  , TableRow
-  ]
-
-type TableRowElements =
-  TableDataCell
-    ': TableHeaderCell
-    ': ScriptSupportingContent
+type ListContent =
+  'ListItem ': ScriptSupportingContent
 
 -- The following lists represent content categories as defined in the HTML
 -- documentation. See here for more information:
@@ -94,268 +85,314 @@ type TableRowElements =
 -- This list represents all elements that are considered metadata content.
 --
 type MetadataContent =
-  [ Base
-  , Link
-  , Meta
-  , NoScript
-  , Script
-  , Style
-  , Title
+  [ 'Base
+  , 'Link
+  , 'Meta
+  , 'NoScript
+  , 'Script
+  , 'Style
+  , 'Title
   ]
 
 -- This list represents all elements that are considered flow content.
 --
 type FlowContent =
-  [ Text
-  , Anchor
-  , Abbreviation
-  , Address
-  , Article
-  , Aside
-  , Audio
-  , Bold
-  , BiDirectionalIsolation
-  , BiDirectionalOverride
-  , Blockquote
-  , Break
-  , Button
-  , Canvas
-  , Cite
-  , Code
-  , Data
-  , DataList
-  , Deletion
-  , Details
-  , Definition
-  , Dialog
-  , Division
-  , DescriptionList
-  , Emphasis
-  , Embed
-  , Fieldset
-  , Figure
-  , Footer
-  , Form
-  , H1
-  , H2
-  , H3
-  , H4
-  , H5
-  , H6
-  , Header
-  , HeadingGroup
-  , HorizontalRule
-  , Italic
-  , IFrame
-  , Image
-  , Input
-  , Insertion
-  , KeyboardInput
-  , Label
-  , Main
-  , Map
-  , Mark
-  -- , Math
-  , Menu
-  , Meter
-  , Nav
-  , NoScript
-  , Object
-  , OrderedList
-  , Output
-  , Paragraph
-  , Picture
-  , PreformattedText
-  , Progress
-  , Quotation
-  , Ruby
-  , Strikethrough
-  , Sample
-  , Search
-  , Script
-  , Section
-  , Select
-  , Slot
-  , Small
-  , Span
-  , Strong
-  , Subscript
-  , Superscript
-  -- , SVG
-  , Table
-  , Template
-  , TextArea
-  , Time
-  , Underline
-  , UnorderedList
-  , Variable
-  , Video
-  , WordBreakOpportunity
+  [ 'Text
+  , 'Anchor
+  , 'Abbreviation
+  , 'ContactAddress
+  , 'Article
+  , 'Aside
+  , 'Audio
+  , 'BringAttentionTo
+  , 'BidirectionalIsolation
+  , 'BidirectionalOverride
+  , 'Blockquote
+  , 'LineBreak
+  , 'Button
+  , 'Canvas
+  , 'Citation
+  , 'Code
+  , 'Data
+  , 'DataList
+  , 'DeletedText
+  , 'Details
+  , 'Definition
+  , 'Dialog
+  , 'Division
+  , 'DescriptionList
+  , 'Emphasis
+  , 'Embed
+  , 'Fieldset
+  , 'Figure
+  , 'Footer
+  , 'Form
+  , 'H1
+  , 'H2
+  , 'H3
+  , 'H4
+  , 'H5
+  , 'H6
+  , 'Header
+  , 'HeadingGroup
+  , 'HorizontalRule
+  , 'IdiomaticText
+  , 'IFrame
+  , 'Image
+  , 'Input
+  , 'InsertedText
+  , 'KeyboardInput
+  , 'Label
+  , 'Main
+  , 'Map
+  , 'Mark
+  --' , Math
+  , 'Menu
+  , 'Meter
+  , 'Nav
+  , 'NoScript
+  , 'Object
+  , 'OrderedList
+  , 'Output
+  , 'Paragraph
+  , 'Picture
+  , 'PreformattedText
+  , 'Progress
+  , 'Quotation
+  , 'Ruby
+  , 'Strikethrough
+  , 'Sample
+  , 'Search
+  , 'Script
+  , 'Section
+  , 'Select
+  , 'Slot
+  , 'SideComment
+  , 'Span
+  , 'Strong
+  , 'Subscript
+  , 'Superscript
+  --' , SVG
+  , 'Table
+  , 'ContentTemplate
+  , 'TextArea
+  , 'Time
+  , 'Underline
+  , 'UnorderedList
+  , 'Variable
+  , 'Video
+  , 'WordBreakOpportunity
   ]
 
 -- This list represents all elements that are considered sectioning content.
 --
 type SectioningContent =
-  [ Article
-  , Aside
-  , Nav
-  , Section
+  [ 'Article
+  , 'Aside
+  , 'Nav
+  , 'Section
   ]
 
 -- This list represents all elements that are considered sectioning content.
 --
 type HeadingContent =
-  HeadingGroup ': Headings
+  'HeadingGroup ': Headings
 
 -- This list represents all elements that are considered phrasing content.
 --
 type PhrasingContent =
-  [ Text
-  , Abbreviation
-  , Audio
-  , Bold
-  , BiDirectionalIsolation
-  , BiDirectionalOverride
-  , Break
-  , Button
-  , Canvas
-  , Cite
-  , Code
-  , Data
-  , DataList
-  , Definition
-  , Emphasis
-  , Embed
-  , Italic
-  , IFrame
-  , Image
-  , Input
-  , KeyboardInput
-  , Label
-  , Mark
-  -- , Math
-  , Meter
-  , NoScript
-  , Object
-  , Output
-  , Picture
-  , Progress
-  , Quotation
-  , Ruby
-  , Strikethrough
-  , Sample
-  , Script
-  , Select
-  , Slot
-  , Small
-  , Span
-  , Strong
-  , Subscript
-  , Superscript
-  -- , SVG
-  , Template
-  , TextArea
-  , Time
-  , Underline
-  , Variable
-  , Video
-  , WordBreakOpportunity
+  [ 'Text
+  , 'Abbreviation
+  , 'Audio
+  , 'BringAttentionTo
+  , 'BidirectionalIsolation
+  , 'BidirectionalOverride
+  , 'LineBreak
+  , 'Button
+  , 'Canvas
+  , 'Citation
+  , 'Code
+  , 'Data
+  , 'DataList
+  , 'Definition
+  , 'Emphasis
+  , 'Embed
+  , 'IdiomaticText
+  , 'IFrame
+  , 'Image
+  , 'Input
+  , 'KeyboardInput
+  , 'Label
+  , 'Mark
+  --' , Math
+  , 'Meter
+  , 'NoScript
+  , 'Object
+  , 'Output
+  , 'Picture
+  , 'Progress
+  , 'Quotation
+  , 'Ruby
+  , 'Strikethrough
+  , 'Sample
+  , 'Script
+  , 'Select
+  , 'Slot
+  , 'SideComment
+  , 'Span
+  , 'Strong
+  , 'Subscript
+  , 'Superscript
+  --' , SVG
+  , 'ContentTemplate
+  , 'TextArea
+  , 'Time
+  , 'Underline
+  , 'Variable
+  , 'Video
+  , 'WordBreakOpportunity
   ]
 
 -- This list represents all elements that are considered embedded content.
 --
 type EmbeddedContent =
-  [ Audio
-  , Canvas
-  , Embed
-  , IFrame
-  , Image
-  -- , Math
-  , Object
-  , Picture
-  -- , SVG
-  , Video
+  [ 'Audio
+  , 'Canvas
+  , 'Embed
+  , 'IFrame
+  , 'Image
+  --' , Math
+  , 'Object
+  , 'Picture
+  --' , SVG
+  , 'Video
   ]
 
 -- This list represents all elements that are considered interactive content.
 --
 type InteractiveContent =
-  [ Button
-  , Details
-  , Embed
-  , IFrame
-  , Label
-  , Select
-  , TextArea
+  [ 'Button
+  , 'Details
+  , 'Embed
+  , 'IFrame
+  , 'Label
+  , 'Select
+  , 'TextArea
   ]
 
 -- This list represents all elements that are considered form-associated
 -- content.
 --
 type FormAssociatedContent =
-  [ Button
-  , Fieldset
-  , Input
-  , Label
-  , Meter
-  , Object
-  , Output
-  , Progress
-  , Select
-  , TextArea
+  [ 'Button
+  , 'Fieldset
+  , 'Input
+  , 'Label
+  , 'Meter
+  , 'Object
+  , 'Output
+  , 'Progress
+  , 'Select
+  , 'TextArea
   ]
 
 -- This list represents all elements that are of the listed subtype of
 -- form-associated content.
 --
 type ListedContent =
-  [ Button
-  , Fieldset
-  , Input
-  , Object
-  , Output
-  , Select
-  , TextArea
+  [ 'Button
+  , 'Fieldset
+  , 'Input
+  , 'Object
+  , 'Output
+  , 'Select
+  , 'TextArea
   ]
 
 -- This list represents all elements that are of the labelable subtype of
 -- form-associated content.
 --
 type LabelableContent =
-  [ Button
-  , Input
-  , Meter
-  , Output
-  , Progress
-  , Select
-  , TextArea
+  [ 'Button
+  , 'Input
+  , 'Meter
+  , 'Output
+  , 'Progress
+  , 'Select
+  , 'TextArea
   ]
 
 -- This list represents all elements that are of the submittable subtype of
 -- form-associated content.
 --
 type SubmittableContent =
-  [ Button
-  , Input
-  , Object
-  , Select
-  , TextArea
+  [ 'Button
+  , 'Input
+  , 'Object
+  , 'Select
+  , 'TextArea
   ]
 
 -- This list represents all elements that are of the resettable subtype of
 -- form-associated content.
 --
 type ResettableContent =
-  [ Input
-  , Output
-  , Select
-  , TextArea
+  [ 'Input
+  , 'Output
+  , 'Select
+  , 'TextArea
   ]
 
 -- This list represents all elements that are considered script-supporting
 -- elements.
 --
 type ScriptSupportingContent =
-  [ Script
-  , Template
+  [ 'Script
+  , 'ContentTemplate
   ]
+
+-- This list represents all elements that are valid under a `<legend>` tag.
+type LegendContent =
+  'H1
+    ': 'H2
+    ': 'H3
+    ': 'H4
+    ': 'H5
+    ': 'H6
+    ': PhrasingContent
+
+-- This list represents all elements that are valid under a `<ruby>` tag.
+type RubyContent =
+  'RubyParenthesis
+    ': 'RubyText
+    ': PhrasingContent
+
+-- This list represents all elements that are valid under a `<summary>` tag.
+type SummaryContent =
+  'HeadingGroup
+    ': 'H1
+    ': 'H2
+    ': 'H3
+    ': 'H4
+    ': 'H5
+    ': 'H6
+    ': PhrasingContent
+
+-- This list represents all elements that are valid under a `<table>` tag.
+type TableContent =
+  [ 'TableCaption
+  , 'TableColumnGroup
+  , 'TableHead
+  , 'TableBody
+  , 'TableRow
+  , 'TableFoot
+  ]
+
+-- This list represents all elements that are valid under a `<tr>` tag.
+type TableRowContent =
+  'TableDataCell
+    ': 'TableHeader
+    ': ScriptSupportingContent
+
+type TableRowOnly =
+  '[ TableRow ]
