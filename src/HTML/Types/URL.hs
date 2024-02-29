8cdbb1e6602081f22edfa85270db70b46fcc856c
AugmenTab <tbaum92@live.com>
2024-02-28 20:42:09 -0600

    Adds RelativeURL type
    This type leverages the Beeline `RouteGenerator` to go from a route
    definition to a method and rendered path. Because we're using the
    routers from Beeline, the route can reflect a route on the server's
    existing router, so it will be flagged by the compiler in the event
    of a change. This means the relative URLs defined in `href`
    attributes won't suddenly break if there's a change to the router.

--- src/HTML/Types/URL.hs
+++ src/HTML/Types/URL.hs
@@ -2,10 +2,16 @@ module HTML.Types.URL
   ( AbsoluteURL
   , absoluteURLFromText
   , absoluteURLToText
+  , RelativeURL
+  , relativeURLFromRoute
+  , relativeURLToText
+  , relativeURLMethod
   ) where
 
 import Beeline.HTTP.Client qualified as B
+import Beeline.Routing qualified as R
 import Data.Text qualified as T
+import Network.HTTP.Types qualified as HTTP
 
 newtype AbsoluteURL = AbsoluteURL B.BaseURI
 
@@ -16,3 +22,17 @@ absoluteURLFromText =
 absoluteURLToText :: AbsoluteURL -> T.Text
 absoluteURLToText (AbsoluteURL url) =
   T.pack $ B.renderBaseURI url
+
+data RelativeURL =
+  RelativeURL
+    { relativeURLMethod :: HTTP.StdMethod
+    , relativeURLPath   :: T.Text
+    }
+
+relativeURLFromRoute :: R.RouteGenerator route -> route -> RelativeURL
+relativeURLFromRoute generator =
+  uncurry RelativeURL . R.generateRoute generator
+
+relativeURLToText :: RelativeURL -> T.Text
+relativeURLToText =
+  relativeURLPath
