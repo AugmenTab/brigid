39ef3e454096154e8c81cf33ef6be61e81174517
AugmenTab <tbaum92@live.com>
2025-07-12 16:15:48 -0500

    Fixes Monoid instance for Class
    A lesson in testing before committing. The derived `mconcat` would
    override the `Semigroup` instance, which caused functions relying
    on the `Monoid` instance to not add the space between classes.

--- src/Brigid/HTML/Types/Class.hs
+++ src/Brigid/HTML/Types/Class.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-
 module Brigid.HTML.Types.Class
   ( Class (Class)
   , classToBytes
@@ -7,6 +5,8 @@ module Brigid.HTML.Types.Class
   ) where
 
 import Data.ByteString.Lazy qualified as LBS
+import Data.Foldable1 (fold1)
+import Data.List.NonEmpty qualified as NEL
 import Data.Text qualified as T
 
 import Brigid.Internal.Render qualified as Render
@@ -14,10 +14,14 @@ import Brigid.Internal.Render qualified as Render
 newtype Class =
   Class
     { classToText :: T.Text
-    } deriving (Eq, Monoid)
+    } deriving (Eq)
 
 instance Semigroup Class where
   Class c1 <> Class c2 = Class $ T.unwords [ c1, c2 ]
+
+instance Monoid Class where
+  mempty  = Class T.empty
+  mconcat = maybe mempty fold1 . NEL.nonEmpty
 
 instance Show Class where
   show = mappend "Class " . T.unpack . classToText
