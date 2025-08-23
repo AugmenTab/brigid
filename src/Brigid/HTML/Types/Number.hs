72e8877559a73ca0a3b878226f4addffc6581fd7
AugmenTab <tbaum92@live.com>
2025-08-23 16:45:57 -0500

    Adds `Number` parser from text

--- src/Brigid/HTML/Types/Number.hs
+++ src/Brigid/HTML/Types/Number.hs
@@ -1,9 +1,12 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Brigid.HTML.Types.Number
   ( Number
   , numberFromIntegral
   , numberFromFractional
   , numberFromReal
   , numberFromScientific
+  , numberFromText
   , numberToBytes
   , numberToText
   , number
@@ -14,6 +17,7 @@ import Data.ByteString.Lazy.Char8 qualified as LBS8
 import Data.Function (on)
 import Data.Scientific qualified as Sci
 import Data.Text qualified as T
+import Text.Read (readMaybe)
 
 data Number =
   Number
@@ -53,6 +57,22 @@ numberFromReal n d =
 
 numberFromScientific :: Sci.Scientific -> Int -> Number
 numberFromScientific = Number
+
+numberFromText :: T.Text -> Either String Number
+numberFromText txt =
+  case T.split (== '.') txt of
+    [ n ] ->
+      case readMaybe $ T.unpack n of
+        Just (i :: Int) -> Right $ numberFromIntegral i
+        Nothing -> Left $ "Could not parse integral Number: " <> T.unpack n
+
+    [ _n, d ] ->
+      case readMaybe $ T.unpack txt of
+        Just (f :: Double) -> Right . numberFromFractional f $ T.length d
+        Nothing -> Left $ "Could not parse fractional Number: " <> T.unpack txt
+
+    _otherwise ->
+      Left $ "Invalid Number: " <> T.unpack txt
 
 numberToBytes :: Number -> LBS.ByteString
 numberToBytes = LBS8.pack . formatNumber
